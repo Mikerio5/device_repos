ada5544452e3072effbf653befced849f97894ad
Michael_D <mikerio162@gmail.com>
2020-04-11 12:14:32 +0200

    test

--- app/static/client.js
+++ app/static/client.js
@@ -7,7 +7,7 @@ function showPicker() {
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
   };
@@ -21,13 +21,13 @@ function analyze() {
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
     true);
-  xhr.onerror = function() {
+  xhr.onerror = function () {
     alert(xhr.responseText);
   };
-  xhr.onload = function(e) {
+  xhr.onload = function (e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      
+
       el("results").style = "display: visible;";
       el("progressbar_1").innerHTML = `${response["results"][0][1]} %`;
       el("progressbar_2").innerHTML = `${response["results"][1][1]} %`;
@@ -39,19 +39,19 @@ function analyze() {
       el("label_3").innerHTML = `${response["results"][2][0]}`;
       el("label_4").innerHTML = `${response["results"][3][0]}`;
 
-      el("progressbar_1").setAttribute('style', 'width:'+Number(response["results"][0][1])+'%')
+      el("progressbar_1").setAttribute('style', 'width:' + Number(response["results"][0][1]) + '%')
       el("progressbar_1").setAttribute('aria-valuenow', response["results"][0][1]);
-      el("progressbar_2").setAttribute('style', 'width:'+Number(response["results"][1][1])+'%')
+      el("progressbar_2").setAttribute('style', 'width:' + Number(response["results"][1][1]) + '%')
       el("progressbar_2").setAttribute('aria-valuenow', response["results"][1][1]);
-      el("progressbar_3").setAttribute('style', 'width:'+Number(response["results"][2][1])+'%')
+      el("progressbar_3").setAttribute('style', 'width:' + Number(response["results"][2][1]) + '%')
       el("progressbar_3").setAttribute('aria-valuenow', response["results"][2][1]);
-      el("progressbar_4").setAttribute('style', 'width:'+Number(response["results"][3][1])+'%')
+      el("progressbar_4").setAttribute('style', 'width:' + Number(response["results"][3][1]) + '%')
       el("progressbar_4").setAttribute('aria-valuenow', response["results"][3][1]);
     }
     el("analyze-button").innerHTML = "Analyze";
   };
-  
-  var ctx = document.getElementById('myChartBar');
+
+  require(['app/static/Chart.js'], function (Chart) {
     var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
@@ -84,23 +84,7 @@ function analyze() {
         }
       }
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 
 
   var fileData = new FormData();
