0515fd78eec919ee58f5e871bbe1529956b303cd
Michael_D <mikerio162@gmail.com>
2020-04-11 13:00:31 +0200

    test

--- app/static/client.js
+++ app/static/client.js
@@ -29,64 +29,42 @@ function analyze() {
       var response = JSON.parse(e.target.responseText);
 
       el("results").style = "display: visible;";
-      el("progressbar_1").innerHTML = `${response["results"][0][1]} %`;
-      el("progressbar_2").innerHTML = `${response["results"][1][1]} %`;
-      el("progressbar_3").innerHTML = `${response["results"][2][1]} %`;
-      el("progressbar_4").innerHTML = `${response["results"][3][1]} %`;
-
-      el("label_1").innerHTML = `${response["results"][0][0]}`;
-      el("label_2").innerHTML = `${response["results"][1][0]}`;
-      el("label_3").innerHTML = `${response["results"][2][0]}`;
-      el("label_4").innerHTML = `${response["results"][3][0]}`;
-
-      el("progressbar_1").setAttribute('style', 'width:' + Number(response["results"][0][1]) + '%')
-      el("progressbar_1").setAttribute('aria-valuenow', response["results"][0][1]);
-      el("progressbar_2").setAttribute('style', 'width:' + Number(response["results"][1][1]) + '%')
-      el("progressbar_2").setAttribute('aria-valuenow', response["results"][1][1]);
-      el("progressbar_3").setAttribute('style', 'width:' + Number(response["results"][2][1]) + '%')
-      el("progressbar_3").setAttribute('aria-valuenow', response["results"][2][1]);
-      el("progressbar_4").setAttribute('style', 'width:' + Number(response["results"][3][1]) + '%')
-      el("progressbar_4").setAttribute('aria-valuenow', response["results"][3][1]);
-    }
-    el("analyze-button").innerHTML = "Analyze";
-
-  var ctx= document.getElementById('myChartBar');
-  var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: [`${response["results"][0][0]}`, `${response["results"][1][0]}`, `${response["results"][2][0]}`, `${response["results"][3][0]}`],
-        datasets: [{
-          label: 'Wahrscheinlichkeit für diesen Typ',
-          data: [Number(response["results"][0][1]), Number(response["results"][1][1]), Number(response["results"][2][1]), Number(response["results"][3][1])],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+      el('myChartBar');
+      var myChart = new Chart(el, {
+        type: 'bar',
+        data: {
+          labels: [`${response["results"][0][0]}`, `${response["results"][1][0]}`, `${response["results"][2][0]}`, `${response["results"][3][0]}`],
+          datasets: [{
+            label: 'Wahrscheinlichkeit für diesen Typ',
+            data: [Number(response["results"][0][1]), Number(response["results"][1][1]), Number(response["results"][2][1]), Number(response["results"][3][1])],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
           }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
         }
-      }
-    });
+      });
 
-  };
-
-
+    };
+  }
   var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
